5fa2e6644f44ba3061dfdc615b9a9ad20095933e
lucadisanto7 <lucadisanto47@gmail.com>
2024-09-25 15:11:14 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Hello World!</title>
+</head>
+
+<body>
+    <h1>Questo è Laravel, quindi</h1>
+    <h2>Hello Wordl!</h2>
+</body>
+
+</html>
